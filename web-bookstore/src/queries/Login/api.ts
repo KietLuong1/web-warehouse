2d5b9cfc6291674f065caa9177fb6f13c2d65dcb
Duc Trong <trong.nguyen.cit20@eiu.edu.vn>
2025-06-26 09:19:16 +0700

    update lgin api and deploy web warehouse to Render

--- web-bookstore/src/queries/Login/api.ts
+++ web-bookstore/src/queries/Login/api.ts
@@ -1,18 +1,18 @@
-import axios from 'axios'
+import axiosAccount from '../../configs/services/http/index'
 import { FieldType } from '../../pages/Login'
 import { ChangePasswordFieldType } from '../../pages/Login/ForgotPassword/ForgotPassword'
 
 // Define the login API function
 export const loginApi = async (credentials: FieldType) => {
   try {
-    const response = await axios.post(`http://localhost:8080/api/v1/auth/login`, credentials)
+    const response = await axiosAccount.post(`/auth/login`, credentials)
     console.log('Login successful api', response.data)
     return response.data
   } catch (error) {
     console.error('Login failed', error)
-    if (axios.isAxiosError(error)) {
-      console.error('Axios error details:', error.response?.data || error.message)
-    }
+    // if (axiosAccount.isaxiosAccountError(error)) {
+    //   console.error('axiosAccount error details:', error.response?.data || error.message)
+    // }
     return null
   }
 }
@@ -22,13 +22,13 @@ export const loginApi = async (credentials: FieldType) => {
 // 1. Verify Email (Send OTP)
 export const verifyEmail = async (email: string): Promise<string | null> => {
   try {
-    const response = await axios.post(`http://localhost:8080/api/v1/forgotPassword/verifyMail/${email}`)
+    const response = await axiosAccount.post(`/forgotPassword/verifyMail/${email}`)
     return response.data
   } catch (error) {
     console.error('Failed to verify email', error)
-    if (axios.isAxiosError(error)) {
-      console.error('Axios error details:', error.response?.data || error.message)
-    }
+    // if (axiosAccount.isaxiosAccountError(error)) {
+    //   console.error('axiosAccount error details:', error.response?.data || error.message)
+    // }
     return null
   }
 }
@@ -36,13 +36,13 @@ export const verifyEmail = async (email: string): Promise<string | null> => {
 // 2. Verify OTP
 export const verifyOtp = async (otp: number, email: string): Promise<string | null> => {
   try {
-    const response = await axios.post(`http://localhost:8080/api/v1/forgotPassword/verifyOtp/${otp}/${email}`)
+    const response = await axiosAccount.post(`/forgotPassword/verifyOtp/${otp}/${email}`)
     return response.data
   } catch (error) {
     console.error('Failed to verify OTP', error)
-    if (axios.isAxiosError(error)) {
-      console.error('Axios error details:', error.response?.data || error.message)
-    }
+    // if (axiosAccount.isaxiosAccountError(error)) {
+    //   console.error('axiosAccount error details:', error.response?.data || error.message)
+    // }
     return null
   }
 }
@@ -50,16 +50,16 @@ export const verifyOtp = async (otp: number, email: string): Promise<string | nu
 // 3. Change Password
 export const changePassword = async (passwordDetails: ChangePasswordFieldType): Promise<string | null> => {
   try {
-    const response = await axios.post(
-      `http://localhost:8080/api/v1/forgotPassword/changePassword/${passwordDetails.email}`,
+    const response = await axiosAccount.post(
+      `/forgotPassword/changePassword/${passwordDetails.email}`,
       passwordDetails
     )
     return response.data
   } catch (error) {
     console.error('Failed to change password', error)
-    if (axios.isAxiosError(error)) {
-      console.error('Axios error details:', error.response?.data || error.message)
-    }
+    // if (axiosAccount.isaxiosAccountError(error)) {
+    //   console.error('axiosAccount error details:', error.response?.data || error.message)
+    // }
     return null
   }
 }
